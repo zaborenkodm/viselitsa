b11b8958d8db0bbc78d0735286a4a8ac5f7e0f18
zaborenkodm <zaborenkodm@gmail.com>
2016-10-30 17:52:21 +0300

    Игра Виселица

---
+++ viselitsa.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+current_path = "./" + File.dirname(__FILE__)
+
+require current_path + "/game.rb"
+require current_path + "/result_printer.rb"
+require current_path + "/word_reader.rb"
+require "unicode_utils/downcase"
+
+printer = ResultPrinter.new
+
+reader = WordReader.new
+begin
+  slovo = reader.read_from_file(current_path + '/data/words.txt')
+rescue SystemCallError
+  puts "Не удалось открыть файл words.txt"
+end
+
+game = Game.new(UnicodeUtils.downcase(slovo))
+
+while game.status == 0 do
+  printer.print_status(game)
+  game.ask_next_letter
+end
